a22950b60b963fd5265672dd08a19d50cab52431
Cathyaa <35276495+Cathyaa@users.noreply.github.com>
2018-02-06 13:46:27 -0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -7,16 +7,15 @@ public class Driver{
         Map world = new Map();
         world.create();
         System.out.println("Hero begins his journey in the Southwest corner of Yore.");
-        
-        boolean play = true;
-        while(play){
-            for(int a = 0; a <= 14; a++){
+        for(int a = 0; a <= 14; a++){
                 for(int b = 0; b <= 14; b++){
                     System.out.print(" [ " + world.map[a][b].name + " ] ");
                     
                 }
                 System.out.println("");
             }
+        boolean play = true;
+        while(play){
             System.out.println("Enter direction(w,a,s,d):");
             String d = kbReader.next();
             if(d.equals("w")||d.equals("a")||d.equals("s")||d.equals("d")){
@@ -27,6 +26,7 @@ public class Driver{
             }else{
                 System.out.println("Enter a valid response.");
             }
+            
         }
     }
 }
