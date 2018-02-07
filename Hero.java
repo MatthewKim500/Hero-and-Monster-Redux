12de46f7b5cb92619318a058e5055af3b412cb52
MatthewKim500 <matthewkim500@gmail.com>
2018-02-07 13:44:57 -0800

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,12 +1,14 @@
 public class Hero{
     public int attack, health, kill, x, y; 
     boolean hasArmor, hasSword;
-    
+    Weapon dagger = new Dagger();
+    Inventory trousers = new Trousers();
+    public int hides = 0;
     public Hero(){
         attack = (int)(Math.random() * 21 + 10);
         health = 100;
         kill = 0;
-        x = 14;
+        x = 9;
         y = 0;
         hasArmor = false;
         hasSword = false;
